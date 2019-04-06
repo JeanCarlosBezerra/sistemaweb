6f0834de62b2ead44365b443014b24a697df6004
RodrigoSiliprandi <rodrigosiliprandi.si@gmail.com>
2019-04-05 22:14:03 -0300

    teste

---
+++ db/migrate/20190406005018_create_professors.rb
@@ -0,0 +1,13 @@
+class CreateProfessors < ActiveRecord::Migration[5.2]
+  def change
+    create_table :professors do |t|
+      t.string :nome
+      t.string :documento
+      t.integer :telefone
+      t.string :email
+      t.string :endereco
+
+      t.timestamps
+    end
+  end
+end
